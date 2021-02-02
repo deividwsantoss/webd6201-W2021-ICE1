8f6d6bf67a55bf5ce04ee7181aea6cdc3395987f
Deivid Santos <deivid.w.santos@gmail.com>
2021-02-02 09:49:02 +0100

    initial commit

---
+++ .history/Scripts/app_20210202094638.js
@@ -0,0 +1,17 @@
+/* Custom JavaScript goes here */
+
+
+// IIFE - 
+//AKA - Anonymous self-excuting function
+
+(function()
+{
+
+
+    function Start()
+    {
+        console.log("App Started...")
+
+    }
+    window.addEventListener("load", Start);
+})();
